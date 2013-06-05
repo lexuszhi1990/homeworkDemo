772075f3f7d3ff33f7583b6aa5b428c5f9710a9a
David <david@beansmile.com>
2013-06-05 15:45:33 +0800

    bishe

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,28 +1,2 @@
 $(function () {
-  $('<div id="sliderspeed"></div>').slider({
-    range: "min",
-    value: 37,
-    min: 1,
-    max: 700,
-    slide: function (event, ui) {
-      // body..
-    }
-  });
-  
-  $('#cart button.left').click(function () {
-      $('#cart button:eq(0)').html("DASD");
-      console.log("left");
-  });
-  $('#cart button.right').click(function () {
-      console.log("right");
-  });
-  $('#cart button.high').click(function () {
-      console.log("high");
-  });
-  $('#cart button.normal').click(function () {
-      console.log("normal");
-  });
-  $('#cart button.low').click(function () {
-      console.log("low");
-  });
 });
--- app/assets/javascripts/websock.js
+++ app/assets/javascripts/websock.js
@@ -2,35 +2,64 @@
 var webSocket ;
 
 $(function(){
-    webSocket = new WebSocket('ws://localhost:8000/');
+  webSocket = new WebSocket('ws://localhost:8000/');
 
-    webSocket.onopen = function(event){
-      console.log('has open...');
-    };
+  webSocket.onopen = function(event){
+    console.log('has open...');
+  };
 
-    webSocket.onmessage = function(event){
-      console.log('ke' + event.data);
-    };
+  webSocket.onmessage = function(event){
+    console.log('message:' + event.data);
+  };
 
-    webSocket.onerror = function(event){
-      console.log('error');
-    };
+  webSocket.onerror = function(event){
+    console.log('error');
+  };
 
-    webSocket.onclose = function(event){
+  webSocket.onclose = function(event){
     console.log('end');
-    $("#chat").append('<br>Connection closed');
-    };
+  };
 
-    $("form#chat_form").submit(function(e){
-      e.preventDefault();
-      var textfield = $("#message");
-      webSocket.send(textfield.val());
-      textfield.val("");
-      });
-    $("form#nick_form").submit(function(e){
-        e.preventDefault();
-        var textfield = $("#nickname");
-        webSocket.send("lexus1");
-    });
-}) 
+  // light
+  $('#cart button.light-1-on').click(function () {
+    console.log("light-1-on");
+    webSocket.send("light-1-on");
+  });
+
+  $('#cart button.light-1-off').click(function () {
+    console.log("light-1-off");
+    webSocket.send("light-1-off");
+  });
+
+  $('#cart button.light-2-on').click(function () {
+    console.log("light-2-on");
+    webSocket.send("light-2-on");
+  });
+
+  $('#cart button.light-2-off').click(function () {
+    console.log("light-2-off");
+    webSocket.send("light-2-off");
+  });
+
+  $('#cart button.light-3-on').click(function () {
+    console.log("light-3-on");
+    webSocket.send("light-3-on");
+  });
+
+  $('#cart button.light-3-off').click(function () {
+    console.log("light-3-off");
+    webSocket.send("light-3-off");
+  });
+
+  $('#cart button.light-4-on').click(function () {
+    console.log("light-4-on");
+    webSocket.send("light-4-on");
+  });
+
+  $('#cart button.light-4-off').click(function () {
+    console.log("light-4-off");
+    webSocket.send("light-4-off");
+  });
+
+});
 
